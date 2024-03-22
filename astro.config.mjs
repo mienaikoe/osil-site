d565601c248573b90afaaa92ee2c944de4069b19
Jesse Lee <mienaikoe@gmail.com>
2024-03-22 15:45:37 -0400

    removals

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-import svelte from "@astrojs/svelte";
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), svelte()]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Our Secret Ingredient Is Love';
+export const SITE_DESCRIPTION = `OSIL is a grassroots culinary collective that shares the delight of eating ethically together. We explore how to make meals more environmentally conscious and more nourishing for all our loved ones. We are 100% plant-based.
+
+OSIL facilitates special culinary experiences, creates educational content, and provides culinary services to mission-aligned community partners. Let’s cook something good up together!
+`;
